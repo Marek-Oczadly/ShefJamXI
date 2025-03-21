2cce4aaea0b85deb75434e1a23b7708fa83c4631
Charlie Sutton <charlie.james.sutton@gmail.com>
2025-03-21 19:37:56 +0000

    MAREK

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init() 
 
 color = (255,255,255) 
-position = (0,0) 
+rect_color = (255,0,0) 
 
 # CREATING CANVAS 
 canvas = pygame.display.set_mode((500,500)) 
@@ -11,15 +11,16 @@ canvas = pygame.display.set_mode((500,500))
 # TITLE OF CANVAS 
 pygame.display.set_caption("Show Image") 
 
-image = pygame.image.load("Marek.jpg") 
+image = pygame.image.load("MAREK.jpg") 
 exit = False
 
 while not exit: 
 	canvas.fill(color) 
-	canvas.blit(image, dest = position) 
-
 	for event in pygame.event.get(): 
 		if event.type == pygame.QUIT: 
 			exit = True
 
+	pygame.draw.rect(canvas, rect_color, 
+					pygame.Rect(30,30,60,60)) 
 	pygame.display.update() 
+
