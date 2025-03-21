4d26ae3a4cda97f838168852c1a830d092d0db7c
Charlie Sutton <charlie.james.sutton@gmail.com>
2025-03-21 19:27:03 +0000

    MAREK

--- game.py
+++ game.py
@@ -2,15 +2,24 @@ import pygame
 
 pygame.init() 
 
+color = (255,255,255) 
+position = (0,0) 
+
 # CREATING CANVAS 
-canvas = pygame.display.set_mode((500, 500)) 
+canvas = pygame.display.set_mode((500,500)) 
 
 # TITLE OF CANVAS 
-pygame.display.set_caption("My Board") 
+pygame.display.set_caption("Show Image") 
+
+image = pygame.image.load("Marek.jpg") 
 exit = False
 
 while not exit: 
+	canvas.fill(color) 
+	canvas.blit(image, dest = position) 
+
 	for event in pygame.event.get(): 
 		if event.type == pygame.QUIT: 
 			exit = True
+
 	pygame.display.update() 
