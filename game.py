2dfbda20a6c960afd40ebb62fd7ca60739856202
Marek Oczadly <moczadly@outlook.com>
2025-03-21 19:57:16 +0000

    Merged

--- game.py
+++ game.py
@@ -11,10 +11,6 @@ canvas = pygame.display.set_mode((500,500))
 # TITLE OF CANVAS 
 pygame.display.set_caption("Show Image") 
 
-<<<<<<< HEAD
-image = pygame.image.load("MAREK.jpg") 
-=======
->>>>>>> 8c297ba4b1cfab152121469f5b61ea8c9eba1fc6
 exit = False
 
 while not exit: 
