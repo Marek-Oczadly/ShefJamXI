378432754b27badae5a3688f05490a83dd4e8b86
LilyNkantah <nkantahlily1@gmail.com>
2025-03-22 02:16:26 +0000

    More tutorial

--- tutorial_info.py
+++ tutorial_info.py
@@ -1,5 +1,6 @@
 #learning pygame
 import pygame
+import random
 
 # --ADDITIONAL NOTES:
 # -- an event is anything that is happening inside your game window i.e. pressing a key
@@ -16,6 +17,9 @@ pygame.display.set_caption("Spacey Vaders")
 icon = pygame.image.load("graphics/galaxy.png")
 pygame.display.set_icon(icon)
 
+#background image
+background = pygame.image.load("graphics/space-background.png")
+
 #ADDING IMAGES INTO GAME WINDOW
 #Player
 playerImg = pygame.image.load("graphics/space-player.png")
@@ -24,20 +28,25 @@ playerY = 380 #y-coord of image
 playerX_change = 0
 playerY_change = 0
 
-
 def player(x,y):
     screen.blit(playerImg, (x, y)) #draws loaded image on screen
 
+#Enemy
+enemyImg = pygame.image.load("graphics/space-enemy.png")
+enemyX = random.randint(0, 736) #enemy spawns at random points in game window
+enemyY = random.randint(0, 536) 
+enemyX_change = 4
+enemyY_change = 4
 
-
-
-
+def enemy(x,y):
+    screen.blit(enemyImg, (x,y))
 
 #CREATING THE GAME LOOP - ensures game doesnt close until close button is pressed
 running = True
 while running: 
-    #background colour
+    #background colour and image
     screen.fill((166, 0, 255))
+    screen.blit(background, (0, 0)) #makes game run slower as image is loaded upon every iteration in the while loop
 
     for event in pygame.event.get(): #checking if close button has been checked
         if event.type == pygame.QUIT:
@@ -47,19 +56,19 @@ while running:
         #if keystroke pressed, checked whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.1
+                playerX_change = -5
             elif event.key == pygame.K_RIGHT:
-                playerX_change = 0.1
+                playerX_change = 5
             elif event.key == pygame.K_UP:
-                playerY_change = -0.1
+                playerY_change = -5
             elif event.key == pygame.K_DOWN:
-                playerY_change = 0.1
+                playerY_change = 5
         if event.type == pygame.KEYUP: #keypress released
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 playerX_change = 0
                 playerY_change = 0
         
-
+    #ADDING BOUNDARIES TO GAME WINDOW (while controlling player and enemy movement)
     playerX += playerX_change
     if playerX <= 0:
         playerX = 0
@@ -70,7 +79,21 @@ while running:
         playerY = 0
     elif playerY >= 536:
         playerY = 536
+
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 4 #enemy moves away from boundary
+    elif enemyX >= 736:
+        enemyX_change = -4
+    
+    enemyY += enemyY_change
+    if enemyY <= 0:
+        enemyY_change = 4
+    elif enemyY >= 536:
+        enemyY_change = -4
+    
     player(playerX, playerY) 
+    enemy(enemyX, enemyY)
     pygame.display.update() #ensures game window is updated everytime something is added
 
         
