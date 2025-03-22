6417f38691225174a307ce9a2dbdee08d1e8282e
Charlie Sutton <charlie.james.sutton@gmail.com>
2025-03-22 15:08:00 +0000

    added combos

--- character.py
+++ character.py
@@ -25,6 +25,12 @@ class Character(pygame.sprite.Sprite):
         self.attacking = False
         self.frame = 0
 
+        # Combo tracking attributes
+
+        self.f_press_count = 0
+        self.combo_timer = 0
+        self.combo_time_limit = 1000
+
     # make it move, given array of keys
     def move(self, direction):
         self.rect = self.rect.move(direction, 0)
@@ -48,6 +54,20 @@ class Character(pygame.sprite.Sprite):
     def combo1(self):
         pass
 
+    def setComboFrame(self, frame, frame_data):
+        for limit, image in frame_data:
+            if frame <= limit:
+                self.setRect(image)
+                return 
+        self.resetAnimation()
+    
+    def resetAnimation(self):
+        self.frame = 0
+
+        self.attacking = False
+
+        self.setRect("graphics/cyborg/cyborg_base.png")
+
     def setRect(self, new_file: str):
         self.image = pygame.image.load(new_file).convert_alpha()
         self.image = pygame.transform.scale_by(self.image, 0.25)
@@ -57,22 +77,44 @@ class Character(pygame.sprite.Sprite):
         return self.rect.bottom >= 300
 
     def update(self, keys):
+        current_time = pygame.time.get_ticks()
+
+        # Check if combo timer should reset
+        if self.combo_timer > 0 and current_time - self.combo_timer > self.combo_time_limit:
+            self.f_press_count = 0
+            self.combo_timer = 0
+
         if not self.attacking:
-            if keys[pygame.K_a]:
+            # Handle key presses for movement and jumping
+            if keys[pygame.K_a]:  # Move left
                 self.move(-5)
-            if keys[pygame.K_d]:
+            if keys[pygame.K_d]:  # Move right
                 self.move(5)
-            if keys[pygame.K_SPACE] and self.jump_frame is None:
+            if keys[pygame.K_SPACE] and self.jump_frame is None:  # Begin jump
                 self.begin_jump()
-            if keys[pygame.K_f]:
-                self.attacking = True
-            
+
+            # Continue jump if in progress
             if self.jump_frame is not None:
                 self.jump()
+
+            # Track 'f' presses for combo
+            if keys[pygame.K_f]:
+                if self.f_press_count == 0:  # First press starts the timer
+                    self.combo_timer = current_time
+                self.f_press_count += 1
+                pygame.time.delay(150)  # Prevent rapid key polling for a single press
+
+            # Trigger combo if the threshold is met
+            if self.f_press_count >= 3:
+                self.attacking = True
+                self.combo_timer = 0
+                self.f_press_count = 0
+
         else:
+            # Perform combo if in attacking state
             self.combo1()
             self.frame += 0.5
-    
+        
     def getImg(self):
         return self.image
     
--- cyborg.py
+++ cyborg.py
@@ -7,19 +7,11 @@ class Cyborg(Character):
         super().__init__(player_name, max_hp, base_image)
     
     def combo1(self):
-        frame = self.getFrame()
-
-        if frame <= 4:
-            self.setRect("graphics/cyborg/blast/cyborg_blast1.png")
-        elif frame <= 9: 
-            self.setRect("graphics/cyborg/blast/cyborg_blast2.png")
-        elif frame <= 14:
-            self.setRect("graphics/cyborg/blast/cyborg_blast3.png")
-        elif frame <= 19:
-            self.setRect("graphics/cyborg/blast/cyborg_blast4.png")
-        elif frame <= 24:
-            self.setRect("graphics/cyborg/blast/cyborg_blast5.png")
-        else:
-            self.frame = 0
-            self.attacking = False
-            self.setRect("graphics/cyborg/cyborg_base.png")
+        frame_data = [
+            (4, "graphics/cyborg/blast/cyborg_blast1.png"),
+            (9, "graphics/cyborg/blast/cyborg_blast2.png"),
+            (14, "graphics/cyborg/blast/cyborg_blast3.png"),
+            (19, "graphics/cyborg/blast/cyborg_blast4.png"),
+            (24, "graphics/cyborg/blast/cyborg_blast5.png")
+        ]
+        self.setComboFrame(self.frame, frame_data)
